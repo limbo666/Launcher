ac275a156149276aa4ccd747e41ebd1a478a1233
limbo666 <ngeorgousis@hotmail.com>
2021-04-20 23:17:23 +0300

    About screen position fix

--- FrmAbout.vb
+++ FrmAbout.vb
@@ -9,6 +9,9 @@
     End Sub
 
     Private Sub FrmAbout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Top = Form1.Top - ((Me.Height - Form1.Height) / 2)
+        Me.Left = Form1.Left - ((Me.Width - Form1.Width) / 2)
+
         LblVer.Text = Application.ProductVersion
     End Sub
 End Class
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("16.0.*")>
 
-<Assembly: AssemblyVersion("0.1.0.25")>
-<Assembly: AssemblyFileVersion("0.1.0.25")>
+<Assembly: AssemblyVersion("0.1.0.26")>
+<Assembly: AssemblyFileVersion("0.1.0.26")>
